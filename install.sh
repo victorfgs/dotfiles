1a670b85a1a91b3ecb3c355d375e1629f8370c11
Victor Felix <victorfgs199@gmail.com>
2023-11-30 12:13:03 -0300

    update install script

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 cat bashrc.additions >> ~/.bashrc
 
-cp ./.gitmessage ~
-cp ./.gitconfig ~
+# cp ./.gitmessage ~
+# cp ./.gitconfig ~
 
 # powerline fonts for zsh agnoster theme
 git clone https://github.com/powerline/fonts.git
@@ -12,7 +12,7 @@ cd fonts
 cd .. && rm -rf fonts
 
 # oh-my-zsh & plugins
-curl -fsSL https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh | zsh || true
+curl -fsSL https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 zsh -c 'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions'
 zsh -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting'
 cp ./.zshrc ~
@@ -23,7 +23,7 @@ cp ./.zshrc ~
 # save current zshrc
 mv ~/.zshrc ~/.zshrc.bak
 
-sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/deluan/zsh-in-docker/master/zsh-in-docker.sh)" -- -t agnoster
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/deluan/zsh-in-docker/master/zsh-in-docker.sh)" -- -t agnoster
 
 # remove newly created zshrc
 rm -f ~/.zshrc
